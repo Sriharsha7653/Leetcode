710bc3b2ab90df381c2d0c8d2b308060b1fed7bd
Sriharsha7653 <sriharsha7653@gmail.com>
2025-12-19 13:45:20 +0530

    Time: 73 ms (24.51%) | Memory: 57.6 MB (47.76%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,25 +1,43 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        Stack<int[]> stack = new Stack<>();
-        int[] res = new int[arr.length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            // use ">=" to deal with duplicate elements
-            while (!stack.isEmpty() && stack.peek()[0] >= arr[i]) {
-                stack.pop();
+        int n =arr.length;
+        int[] nse=nsefunc(arr);
+        int[] pse=psefunc(arr);
+        int mod=(int)1e9+7;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int left=i-pse[i];
+            int right=nse[i]-i;
+            long freq=left*right*1l;
+            int val=(int)((freq*arr[i])%mod);
+            sum=(sum+val)%mod;
+        }
+        return sum;
+    }
+    public int[] nsefunc(int[] arr){
+        int n=arr.length;
+        int[] ans= new int[n];
+        Stack<Integer> st= new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&arr[st.peek()]>=arr[i]){
+                st.pop();
             }
-
-            int j = stack.isEmpty() ? -1 : stack.peek()[1];
-            res[i] = stack.isEmpty() ? arr[i] * (i + 1) : res[j] + arr[i] * (i - j);
-            stack.push(new int[]{arr[i], i});
+            ans[i]=!st.isEmpty()?st.peek():n;
+            st.push(i);
         }
-
-        long MOD = (long) 1000000007;
-        long ans = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            ans = (ans + (long) res[i]) % MOD;
+        return ans;
+    }
+    public int[] psefunc(int[] arr){
+        int n=arr.length;
+        int[] ans= new int[n];
+        Stack<Integer> st= new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&arr[st.peek()]>arr[i]){
+                st.pop();
+                }
+                ans[i]=!st.isEmpty()?st.peek():-1;
+                st.push(i);
         }
-        return (int) ans;        
+        return ans;
     }
 }
