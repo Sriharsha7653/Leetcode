be9bc13bf731bf2edf2c450259d74501a96362e6
Sriharsha7653 <sriharsha7653@gmail.com>
2025-12-22 21:15:10 +0530

    Time: 3 ms (83.51%) | Memory: 43.6 MB (8.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st= new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='['||c=='{'||c=='('){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){return false;}
+                if(c==')'){
+                    if(st.peek()=='('){
+                        st.pop();
+                    }
+                    else{return false;}
+                }
+                if(c==']'){
+                    if(st.peek()=='['){st.pop();}
+                    else{return false;}
+                }
+                if(c=='}'){
+                    if(st.peek()=='{'){st.pop();}
+                    else{return false;}
+                }
+            }
+          
+        }
+          return st.isEmpty();
+    }
+}
