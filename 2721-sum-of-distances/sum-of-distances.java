b29750a736aa00fb1b70b6e820ade7b4cc6126c5
Sriharsha7653 <sriharsha7653@gmail.com>
2026-02-03 11:58:21 +0530

    Time: 19 ms (91.33%) | Memory: 124.7 MB (26.59%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long[] distance(int[] nums) {
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(!hm.containsKey(nums[i])) {
+                hm.put(nums[i], new ArrayList<>());
+            }
+            hm.get(nums[i]).add(i);
+        }
+        
+        long[] ans = new long[nums.length];
+        for(ArrayList<Integer> indices : hm.values()) {
+            int n = indices.size();
+            long sum = 0;
+            for(int i = 0; i < n; i++) {
+                sum += indices.get(i);
+            }
+            long leftsum = 0;
+            long rightsum = sum;
+            long currsum = 0;
+            for(int i = 0; i < n; i++) {
+                currsum = 0;
+                currsum += (long)i*(long)indices.get(i) - leftsum;
+                currsum += rightsum - (long)(n-i)*(long)indices.get(i);
+                leftsum += (long)indices.get(i);
+                rightsum -= (long)indices.get(i);
+                ans[indices.get(i)] = currsum;
+            }
+        }
+        return ans;
+    }
+}
